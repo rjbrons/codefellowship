173f52512ef8b183a68f021bc78ea51650d8c988
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-17 15:04:49 -0700

    initial commit

---
+++ src/main/java/com/rjb888/fellowship/codefellowship/CodefellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.rjb888.fellowship.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rjb888/fellowship/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rjb888.fellowship.codefellowship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodefellowshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
